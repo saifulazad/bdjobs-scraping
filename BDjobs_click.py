6741a42ee7744908ebc6bfdd24c65474447578b7
Your Name <you@example.com>
2017-01-05 21:51:17 +0600

    CROSS platform supprt

--- BDjobs_click.py
+++ BDjobs_click.py
@@ -1,8 +1,16 @@
 __author__ = 'Azad'
+import sys
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+import config
+
+try:
+    config.init_path()
+except Exception as ex:
+    print(ex)
+    sys.exit()
 file = open('link.txt', 'a')
 
 def extract_links_from_pages(page):
@@ -15,7 +23,9 @@ def extract_links_from_pages(page):
         print(val.get('href'))
 
 
-driver = webdriver.Chrome('chromedriver.exe')
+chrome_driver = config.PATH_WITH_DRIVER
+
+driver = webdriver.Chrome(chrome_driver)
 driver.get("http://jobs.bdjobs.com/jobsearch.asp?fcatId=8")
 extract_links_from_pages(driver.page_source)
 for x in range(2, 9):
--- config.py
+++ config.py
@@ -1,7 +1,37 @@
 import platform
+
+
 os = platform.system()
 
-golbal_os = ('Windows', 'Linux')
+golbal_os = ('Windows', 'Linux') # List of OSs we are supporting now
 
-PATH_WITH_DRIVER = ''
+PATH_WITH_DRIVER = '' # Default PATH of chrome driver
 
+def init_path():
+    """
+    This is the function that will work to select driver based on OS ('Windows', 'Linux')
+    We are only supporting only 'Windows', 'Linux'. Hope next we will support OSX
+    :return: True  if chrome driver found
+    :Exception: otherwise
+    """
+    global PATH_WITH_DRIVER
+    global os
+    if os is 'Windows':
+        PATH_WITH_DRIVER = 'C:\chromedriver.exe'
+    elif os is 'Linux':
+        PATH_WITH_DRIVER = ''
+
+    import os.path
+    is_consist = os.path.exists(PATH_WITH_DRIVER)
+
+    if is_consist is True:
+        return True
+    else:
+        raise Exception('No chromedriver found at {0}.\nPlease be sure you have configure PATH_WITH_DRIVER'.format(PATH_WITH_DRIVER))
+
+
+
+if __name__ == "__main__":
+
+    print(init_path())
+    print(os)
