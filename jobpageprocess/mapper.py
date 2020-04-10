199ff664d626b1d79d1643887b40c76638fba463
azad <mr.saiful.azad@gmail.com>
2020-04-10 08:22:08 +0600

    add chrome driver

--- jobpageprocess/mapper.py
+++ jobpageprocess/mapper.py
@@ -1,13 +1,6 @@
 from bs4 import BeautifulSoup
 
-from firebase import firebase
-
-import config
-from fire_base_config import CONFIG
-from dateUtil import get_today_file_name
-
 from jobpageprocess import fetch_page
-from jobpageprocess import Reader
 
 
 class Mapper(object):
@@ -109,16 +102,6 @@ class Mapper(object):
 
 
 if __name__ == '__main__':
-
-    txt_file_name = get_today_file_name()
-    firebase = firebase.FirebaseApplication(CONFIG['URL'], None)
-    read_file = config.TXT_FILE_PATH + txt_file_name
-
-    val = (Reader(file_name=read_file).get_file_content())
-    file_name = get_today_file_name().split('.')[0]
-    for x in val[20:]:
-        job_id = (x.split('id=')[1].split('&')[0])
-        page = fetch_page('http://jobs.bdjobs.com/' + x)
-        ob = Mapper(page=page)
-        firebase.put('/job/',job_id ,(ob._read_from_HTML()))
-        print(job_id)
+    page = fetch_page('https://jobs.bdjobs.com/jobdetails.asp?id=904199&fcatId=8&ln=1')
+    ob = Mapper(page=page)
+    print(ob._read_from_HTML())
